0b6ff6ba433889c67fe34e8db41dea9acfd33573
Kodai Yamamoto <kyama8209@gmail.com>
2020-07-29 17:00:22 +0900

    practice15

--- ruby.rb
+++ ruby.rb
@@ -1,10 +1,8 @@
-hash = {}
-hash[:name] = "suzuki"
-hash[:height] = 1.6
-hash[:weight] = 60
-hash[:bmi] = (hash[:weight] / hash[:height] ** 2).round(1)
+str = "is this a pen?"
 
-
-hash.each do |key, value|
-  puts "#{key}: #{value}"
+if str.include?("?")
+  puts "yes, it is."
 end
+
+# ワンライナー
+puts "yes, it is." if str.include?("?")
