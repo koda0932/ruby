90773d835d716832ce5d9593fb517ef090aa2376
Kodai Yamamoto <kyama8209@gmail.com>
2020-08-02 09:58:54 +0900

    practice 60

--- ruby.rb
+++ ruby.rb
@@ -1,13 +1,12 @@
-def sleep_in(weekday, vacation)
-  if (weekday != true) || (vacation == true)
-    puts "True"
-  else
-    puts "False"
-  end
+def missing_char(array, n)
+  array.slice!(n)
+  puts array
 end
 
+missing_char('abcdefg', 2..4)      #=> "abfg"
+missing_char('abcdefg', 2)         #=> "abdefg"
 
-weekday = true
-vacation = false
 
-sleep_in(weekday,vacation)
+# slice!
+# 指定した範囲 (String#[] 参照) を文字列から取り除いたうえで取り除いた部分文字列を返します。
+# 引数が範囲外を指す場合は nil を返します。
