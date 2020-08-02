de42aa411cf910e297f1bd92849f102d02567e2d
Kodai Yamamoto <kyama8209@gmail.com>
2020-08-02 10:51:24 +0900

    practice 61

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,6 @@
-def missing_char(array, n)
-  array.slice!(n)
-  puts array
+def ary(nums)
+  result = nums.count(9)
+  puts result
 end
 
-missing_char('abcdefg', 2..4)      #=> "abfg"
-missing_char('abcdefg', 2)         #=> "abdefg"
-
-
-# slice!
-# 指定した範囲 (String#[] 参照) を文字列から取り除いたうえで取り除いた部分文字列を返します。
-# 引数が範囲外を指す場合は nil を返します。
+ary([1, 4, 5, 9, 7, 9])   #=> 2
