d324ac9056b21af5689c569056bf9af59d566959
Kodai Yamamoto <kyama8209@gmail.com>
2020-08-03 15:33:25 +0900

    practice68

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,11 @@
-def near_ten(num)
-  result = num % 10
-  if result <= 2 || result >= 8
+def into(num, outside)
+  if (num >= 1 && num <= 10) || outside == true
     puts "true"
   else
     puts "false"
   end
 end
 
+into(11, false)
 
-near_ten(11)
+
