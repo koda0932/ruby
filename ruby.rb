b6f554b658926e55ddf6326bcaa2a4c576665a1a
Kodai Yamamoto <kyama8209@gmail.com>
2020-08-03 15:07:01 +0900

    practice67

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,11 @@
-def left(moji)
-  puts moji.slice!(0, 2)
+def near_ten(num)
+  result = num % 10
+  if result <= 2 || result >= 8
+    puts "true"
+  else
+    puts "false"
+  end
 end
 
-left("adkos")
 
-# => ad
+near_ten(11)
