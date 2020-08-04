4c14db727823eedaceb815d1d660a30868c26916
Kodai Yamamoto <kyama8209@gmail.com>
2020-08-04 13:05:55 +0900

    practice 74

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,5 @@
-def close(a,b,c)
-  x = (a-b).abs
-  y = (a-c).abs
-  z = (b-c).abs
-  if (x == 1 || y == 1) && (z >= 2)
-    puts "true"
-  else
-    puts "false"
-  end
+def count_code(str)
+  puts str.scan(/co.e/).length
 end
 
-close(4,1,3)
+count_code('aaacosebbb')
